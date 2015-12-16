e771e48177d8bfe307dac8e12c9f52a44742ffc0
Marco <tentimarco0@gmail.com>
2015-12-16 22:13:30 +0100

    2015-12-16v2

--- testWitSources/lib/main.js
+++ testWitSources/lib/main.js
@@ -1,11 +1,9 @@
 
-/* GITHUB dependency */
+
 import L from '../jspm_packages/github/Leaflet/leaflet@0.7.7.js';
 import '../jspm_packages/github/Leaflet/Leaflet.markercluster@0.4.0.js';
 import '../jspm_packages/npm/leaflet-omnivore@0.3.2.js';
 
-
-/* NPM dependency */
 /*http://stackoverflow.com/questions/33795493/load-font-awesome-with-jspm*/
 import '../jspm_packages/npm/font-awesome@4.5.0/css/font-awesome.css!';
 
